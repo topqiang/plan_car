e5b931091ea54acb1f0b915ab20be77067e9b0e0
topqiang <topqiang@kanziqiang.top>
2017-03-29 09:13:47 +0800

    0329

--- Application/Home/Controller/OrderController.class.php
+++ Application/Home/Controller/OrderController.class.php
@@ -80,7 +80,7 @@ class OrderController extends BaseController{
 
 			$mesreturn = $this -> curl(json_encode($json),$url);
 
-			ajaxReturn("success","添加成功！",$mesreturn);
+			ajaxReturn("error","添加成功！",$mesreturn);
 		}else{
 			ajaxReturn("error","添加失败！");
 		}
